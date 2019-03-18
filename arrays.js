19bccf06a0055de1a0425e2f5c887b22a22c3af8
Franklin <makyfj@hotmail.com>
2019-03-18 15:54:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromEndOfArray(array, element) {
   return array;
 } 
 
-function removeElementFromEndOfArray(array, element) {
+function removeElementFromEndOfArray(array) {
 	array.slice(0, array.length - 1);
 	return array;
 }
